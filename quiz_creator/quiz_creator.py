8e476c5d87795329e08f88236287f9ec01ecacc2
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-05-28 09:19:07 +0800

    paste the quiz creator class

--- quiz_creator/quiz_creator.py
+++ quiz_creator/quiz_creator.py
@@ -1,3 +1,29 @@
 from quiz_base_questions import QuizBaseQuestions
 from colorama import Fore
 
+class QuizCreator(QuizBaseQuestions):
+    def add_questions(self):
+        while True:
+            print(Fore.GREEN + "\nAdding a new question")
+            question_text = input("Enter the question: ")
+            choice_a = input("a.")
+            choice_b = input("b.")
+            choice_c = input("c.")
+            choice_d = input("d.")
+            answer = input("Correct answer a/b/c/d/:").strip().lower()
+
+            if answer not in ('a', 'b', 'c', 'd'):
+                print(Fore.RED + "Invalid answer")
+                continue
+
+            with open(self.filename, 'a') as file:
+               file.write(f"Question:{question}\n")
+            file.write(f"A. {a}\n")
+            file.write(f"B. {b}\n")
+            file.write(f"C. {c}\n")
+            file.write(f"D. {d}\n")
+            file.write(f"Answer: {answer.upper()}\n\n")
+            
+            again = input("Add a new question? (yes/no):").strip().lower()
+            if again != 'yes':
+                break 
